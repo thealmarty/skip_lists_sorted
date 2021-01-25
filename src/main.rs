b169805505554112c29fd6f8f9cef819f1207dce
Marty Stumpf <thealmartyblog@gmail.com>
2021-01-25 09:56:37 -0800

    Abstract out the element type of the list.

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,17 @@ fn main() {
     println!("node1 is {:?}", node1);
 }
 
-fn build_node(ele: usize, next: Vec<i32>, height: i32) -> Node {
+fn build_node(ele: i32, next: Vec<i32>, height: u32) -> Node {
     Node { ele, next, height }
 }
+
+type EleType = i32;
 
 #[derive(Debug)]
 // A node containing an element and a list of pointers.
 struct Node {
-    ele: usize, // the element
+    ele: EleType, // the element
     // a list of pointers, pointing to the node's successor at a given level i
     next: Vec<i32>,
-    height: i32, // the current height
+    height: u32, // the current height
 }
