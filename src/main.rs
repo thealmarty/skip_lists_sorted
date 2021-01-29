64298ce32403180f90a7baedb72ab0f956de26fe
Marty Stumpf <thealmartyblog@gmail.com>
2021-01-29 12:09:32 -0800

    Fix find_node. Put nodes in arc.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 // use fastrand::bool; // for coin flipping
-// use std::cmp::Ordering; // TODO ordering of val required.
-//                         // TODO change to atomic nodes to allow for concurrency
+// TODO change to atomic nodes to allow for concurrency
 // use std::borrow::Borrow;
+use std::sync::Arc;
 
 fn main() {}
 
@@ -12,25 +12,30 @@ struct Node<T: Ord> {
     next: VecOfNodes<T>, // a list of next nodes
 }
 
+// a node with atomic reference counting
+type ArcNode<T> = Arc<Node<T>>;
+
 // a vector of successive nodes, at a given level i.
 // the successive node could be none.
-type VecOfNodes<T> = Vec<Option<Node<T>>>;
+type VecOfNodes<T> = Vec<Option<ArcNode<T>>>;
 
 // a skip list is a vec of nodes.
-type SkipList<T> = Vec<Node<T>>;
+type SkipList<T> = Vec<ArcNode<T>>;
 
 // finds if val is in the slist. If not, it returns none. Otherwise, it returns
 // the node that has val equals to the input val.
-fn find_node<T: Ord>(val: T, slist: SkipList<T>) -> Option<Node<T>> {
+fn find_node<T: Ord>(val: T, slist: SkipList<T>) -> Option<ArcNode<T>> {
     if slist.len() == 0 {
         None
     } else {
-        let mut cur_height = slist.len() - 1; // current height
-                                              // moving off the sentinel
+        // current height
+        let mut cur_height = slist.len() - 1;
+        // moving off the sentinel
         while cur_height > 0 && slist[cur_height].val > val {
             cur_height -= 1;
         } // moved off the sentinel
-        let mut cur_node = &slist[cur_height];
+          // current node to compare to
+        let mut cur_node = slist[cur_height].clone();
         while cur_height > 0 {
             match &cur_node.next[cur_height] {
                 None => cur_height -= 1,
@@ -38,19 +43,20 @@ fn find_node<T: Ord>(val: T, slist: SkipList<T>) -> Option<Node<T>> {
                     if node.val > val {
                         cur_height -= 1;
                     } else {
-                        cur_node = node;
+                        cur_node = node.clone();
                     }
                 }
             }
         }
         if cur_node.val == val {
-            None // fix this Some(cur_node)
+            Some(cur_node.clone())
         } else {
             None
         }
     }
 }
 
+// TODO write tests
 // fn add<T>(val: T, mut slist: SkipList<T>) -> SkipList<T> {
 //     if true {
 //     //TODO find_node<T>(val, slist) == None { // if val doesn't exist, add it
